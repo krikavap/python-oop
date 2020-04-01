afb2ad51e4034227b580d7cc69c70b19542915de
krikavap <39693113+krikavap@users.noreply.github.com>
2020-04-01 19:39:29 +0200

    add argument for multidimensional cube

--- arena.py
+++ arena.py
@@ -7,13 +7,16 @@ class Kostka:
     Trida reprezentuje hraci kostku
     """
 
-    def __init__(self):
+    def __init__(self, pocet_sten = 6):     # nová definice, přidání argumentu pocet_sten, výchozí hodnota je 6
+    #def __init__(self):    # původní definice
         """
         iniciacni metoda
+        argument pocet_sten určuje, kolik stěn bude objekt mít
         """
         #self.pocet_sten = 6     # takto je atribut veřejný a lze jej měnit zvenku třídy
                                 # např. kostka.pocet_sten = 12
-        self.__pocet_sten = 6   # dvojité podtržítko před názvem udělá z atributu atribut neveřejný 
+        #self.__pocet_sten = 6   # dvojité podtržítko před názvem udělá z atributu atribut neveřejný 
+        self.__pocet_sten = pocet_sten
 
 
     def vrat_pocet_sten(self):
@@ -23,10 +26,9 @@ class Kostka:
         return self.__pocet_sten
 
 
-kostka = Kostka()
-print(kostka.vrat_pocet_sten())
+sestistena = Kostka()   # původní definice s prázdným konstruktorem
+print(sestistena.vrat_pocet_sten())
 
 steny = int(input("zadej počet stěn: "))
-kostka.__pocet_sten = steny
-print(kostka.__pocet_sten)
+kostka = Kostka(steny)      # nová definice instance, kdy určíme počet stěn nového objektu
 print(kostka.vrat_pocet_sten())
