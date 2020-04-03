c60458c8441f273e9762f3a2cfb11d73e6c3ebbb
krikavap <39693113+krikavap@users.noreply.github.com>
2020-04-03 16:12:37 +0200

    add first fight

--- arena.py
+++ arena.py
@@ -48,6 +48,135 @@ class Kostka:
         text = str("Kostka("+str(self.__pocet_sten)+")")
         return text
 
+
+class Bojovnik:
+    """
+    Trida reprezentuje bojovnika do areny
+    """
+    def __init__(self, jmeno, zivot, utok, obrana, kostka):
+        """
+        jmeno - jmeno bojovnika
+        zivot - maximalni zivot bojovnika
+        utok - utok bojovnika
+        obrana - obrana bojovnika
+        kostka - instance kostky
+        
+        zprava - zpráva na konzoli
+        """
+        self.__jmeno = jmeno
+        self.__zivot = zivot
+        self.__max_zivot = zivot
+        self.__utok = utok
+        self.__obrana = obrana
+        self.__kostka = kostka
+        self.__zprava = ""
+
+    def __str__(self):
+        """
+        vrati jmeno bojovnika
+        """
+        return str(self.__jmeno)
+
+    
+    def __repr__(self):
+        """
+        vraci v retezci kod konstruktoru pro funkci eval(), tj vytvoreni kopie bojovnika
+        """
+        text = str("Bojovnik(\""+self.__jmeno+"\","+str(self.__max_zivot)+","+str(self.__utok)+","+str(self.__obrana)+",\""+(self.__kostka)+"\")")
+        return text
+
+    
+    @property               # dekorátor = mění metodu na vlastnost /atribut
+    def nazivu(self):
+        """
+        test, zda je bojovnik nazivu, tzn. zda jeho aktualni zivot je > 0
+        """
+        if self.__zivot > 0:
+            return True
+        else:
+            return False
+
+    def graficky_zivot(self):
+        """
+        graficky vyjadri mnozstvi zbyvajiciho zivota bojovnika
+        """
+        celkem = 20     # max. pocet znaku pro delku zivota 
+        #self.__zivot = 50
+        pocet = int(self.__zivot/self.__max_zivot * celkem)     # delka zivota
+        if (pocet == 0 and self.nazivu):                        # pokud je hodnota života malá, ale bojovník je naživu, dáme pocet = 1
+            pocet = 1
+        return (("["+"#" * pocet) + (" " * (celkem - pocet)+"]" ))           # vrátí počet křížků dle aktuálního života bojovníka
+            
+
+    def bran_se(self, uder):
+        """
+        metoda vypocte velikost zbyvajiciho zivota po utoku
+        pokud je zraneni vetsi nez zbyvajici zivot, vrati zivot = 0
+        """
+        zraneni = uder - (self.__obrana + self.__kostka.hod())       # výpočet zranění
+
+        if zraneni > 0:         # pokud je útok silnější než naše obrana, jsme zranění. Pokud je obrana stejná nebo silnější než útok, na zbývajícím životě se nic nemění
+            zprava = (f"{self.__jmeno} utrpěl poškození {zraneni} hp")          # sestavení zprávy pro konzoli
+            self.__zivot = self.__zivot - zraneni       # výpočet zbývajícího života
+        
+            if self.__zivot < 0:
+                self.__zivot = 0        
+                zprava = zprava + "a zemřel"        # doplnění zprávy
+        else:
+            zprava = (f"{self.__jmeno} odrazil útok ")      # pokud odražení útoku bylo úspěšné a bez zranění
+        
+        self.__nastav_zpravu(zprava)        # vepíše zprávu do vlastnosti
+
+        
+    def utoc(self, souper):
+        """
+        vypocita silu uderu a zautoci
+        argument souper je instance bojovnika, na ktereho se utoci
+        """
+        uder = self.__utok + self.__kostka.hod()
+        
+        zprava = (f"{self.__jmeno} útočí s úderem za {uder} hp ") # sestavení zprávy pro konzoli
+        self.__nastav_zpravu(zprava)
+        
+        souper.bran_se(uder)        
+
+
+    def __nastav_zpravu(self, zprava):
+        """
+        privatni metoda nastavujici zpravu
+        argument je textovy retezec, ktery ma byt vlozen do vlastnosti self.__zprava
+        """
+        self.__zprava = zprava   
+
+    def vrat_posledni_zpravu(self):
+        return self.__zprava
+        
+
+kostka = Kostka(10)         # vytvoření objektu kostka
+bojovnik = Bojovnik("Zalgoren", 100, 20, 10, kostka)        # vytvoření bojovníka
+print(f"Bojovník: {bojovnik}")
+print(f"Naživu: {bojovnik.nazivu}")
+print(f"Život:          {bojovnik.graficky_zivot()} ")
+
+# vytvoření soupeře
+souper = Bojovnik("Shadow", 60, 18, 15, kostka)
+print(f"Bojovník: {bojovnik}")
+print(f"Naživu: {bojovnik.nazivu}")
+print(f"Život:          {bojovnik.graficky_zivot()} ")
+
+# útok soupeře na bojovníka
+souper.utoc(bojovnik)
+print(souper.vrat_posledni_zpravu())
+print(bojovnik.vrat_posledni_zpravu())
+print(f"Život po útoku: {bojovnik.graficky_zivot()} ")
+
+
+
+
+
+
+
+"""
 # vytvoření kostek
 sestistena = Kostka()   # původní definice s prázdným konstruktorem
 steny = int(input("zadej počet stěn: "))
@@ -67,3 +196,4 @@ for _ in range(10):
 jina_sestistena = eval(repr(sestistena))    # duplikuje objekt
 print()
 print(jina_sestistena)
+"""
