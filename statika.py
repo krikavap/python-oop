b62599baa6d5d293803fa21dff695219a0ae0b38
krikavap <39693113+krikavap@users.noreply.github.com>
2020-04-05 20:01:29 +0200

    add static methods and class Trida

--- statika.py
+++ statika.py
@@ -26,22 +26,47 @@ class Uzivatel:
     def __str__(self):
         return self.jmeno
 
-passw = ""
-print("Registrace")
-name = input("Jméno: ")
-delka = 0
+class Trida:
+    # může exisovat i statická metoda např. pro validaci hesla 
+    # a klasické funkce v třídě jako statické metody
+
+    @staticmethod
+    def zvaliduj_heslo(heslo):
+        if len(heslo) < Uzivatel.minimalni_delka_hesla:
+            print(f"Heslo musí být dlouhé minimálně {Uzivatel.minimalni_delka_hesla} znaků")
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def registrace():
+        print("Registrace")
+        name = input("Jméno: ")
+        passw = input("Heslo: ")
+        while Trida.zvaliduj_heslo(passw) == False:          # volá statickou metodu pro validaci hesla
+            passw = input("Heslo: ")
+        
+        xxx = Uzivatel(name, passw)             # založí instanci objektu Uživatel
+        return xxx                              # vrátí ukazatel na novou instanci třídy Uživatel
+
+# registrace nového uživatele
+novy_uz = Trida.registrace()
+print(novy_uz)
+
+# přihlášení nového uživatele
+print("Přihlášení")
+passw = input("Zadej heslo:")
+prihlaseni = novy_uz.prihlas_se(passw)
+if prihlaseni:
+    print("úspěšné přihlášení")
+else:
+    print("neúspěšné přihlášení")
+
+"""
+# tohle je varianta kontroly délky hesla na statickou proměnnou
 while delka < Uzivatel.minimalni_delka_hesla:
     passw = input("Heslo: ")
     delka = len(passw)
     if delka < Uzivatel.minimalni_delka_hesla:
         print(f"Heslo musí být dlouhé minimálně {Uzivatel.minimalni_delka_hesla} znaků")
-pepa = Uzivatel(name, passw)
-print(pepa)
-
-print("Přihlášení")
-passw = input("Zadej heslo:")
-prihlaseni = pepa.prihlas_se(passw)
-if prihlaseni:
-    print("úspěšné přihlášení")
-else:
-    print("neúspěšné přihlášení")
+"""
