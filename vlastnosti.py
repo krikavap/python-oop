cdbda66df887c15c62b1e5f0e858a5aabfe334d7
krikavap <39693113+krikavap@users.noreply.github.com>
2020-04-07 07:11:22 +0200

    add @property

--- vlastnosti.py
+++ vlastnosti.py
@@ -1,20 +1,15 @@
 """
 vlastnosti.py
 příklady třídy a vlastnosti třídy
-druhá verze s privátními atributy třídy a s metodami, kterými lze atributy zvenku číst (gettery) a měnit (settery)
+třetí verze s privátními atributy třídy a s vlastnostmi @property a novým setterem @vek.setter
+podobně lze vytvořit i getter @vek.getter , který např. zajistí nějakou další logiku (zalogování přístupu k údaji apod.)
 """
 class Student:
     def __init__(self, jmeno, muz, vek):
-        """
-        self.jmeno = jmeno                  # veřejně přístupné atributy
-        self.muz = muz
-        self.vek = vek
-        self.plnolety = (vek > 18)      # vlastnost?
-        """
         self.__jmeno = jmeno                  # privátní atributy
         self.__muz = muz
         self.__vek = vek
-        self.__plnolety = (vek > 18)      
+        self.__plnolety = (vek >= 18)      
 
     def __str__(self):
         jsem_plnolety = "jsem" if self.__plnolety else "nejsem"       # to je hezká zkratka s if
@@ -25,14 +20,21 @@ class Student:
     def vrat_jmeno(self):               # metoda vracející hodnotu atributu, tzv. getter
         return self.__jmeno
 
-    def vrat_plnoletost(self):
+    @property                           # to samé jako getter, ale pomocí property - nastavení vlastnosti (@property = dekonátor)
+    def jmeno(self):
+        return self.__jmeno             # atribut svázaný s metodou
+
+    def vrat_plnoletost(self):          # getter
         return self.__plnolety
 
-    def vrat_vek(self):
+    def vrat_vek(self):                 # getter
         return self.__vek
 
-    def vrat_muz(self):
+    def vrat_muz(self):                 # getter
         return self.__muz
+
+    def nastav_muz(self, muz):          # setter
+        self.__muz = muz
 
     # metoda ošetří i případ, že věk < 18 a je nutné změnit i atribut plnoletost
     def nastav_vek(self, vek):      # metoda měnící atribut tzv. setter
@@ -42,18 +44,26 @@ class Student:
         else:
             self.__plnolety = True
 
-    def nastav_muz(self, muz):
-        self.__muz = muz
+    @property                           # dekonátor property
+    def vek(self):
+        return self.__vek
+
+    @vek.setter                         # dekonátor setter - mění vlastnost, může obsahovat i logiku (zde test plnoletosti)
+    def vek(self, hodnota):
+        self.__vek = hodnota
+        self.__plnolety = (hodnota >= 18)
 
     
 s = Student("Jiří Hora", True, 29)
 print(s)
-s.vek = 15                  # public atributy nelze zvenku měnit a způsobit nekonzistenci. 
-s.muz = False               
-print(s)                # výstup stejný jako první, i když jsme chtěli argumenty změnit
-s.nastav_vek(12)
-s.nastav_muz(False)
-print(s)                # výstup se správně změněnými atributy
+s.vek = 15                  # pomocí setteru vlastnosti vek lze private atribut vek zvenku řízeně změnit 
+s.muz = False               # privátní atribut není možné bez setteru změnit
+print(s)                    
+s.nastav_vek(12)            # změna pomocí původního setteru nastav_vek()
+print(s)                    # výstup se správně změněnými atributy
+
+print(s.jmeno)              # vytvořením property lze vypsat privat jmeno
+print(s.vek)                # vytvořením property lze vypsat privat vek
 
 s2 = Student("Jiřina Nová Hora", False, 15)
 print(s2)
